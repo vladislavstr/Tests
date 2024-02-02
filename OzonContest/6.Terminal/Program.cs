675e6143e5682c3106d656e4876df05ffae5a39a
VladW <VIStrelnikov94@gmail.com>
2024-02-02 19:47:50 +0300

    created 6.Terminal

---
+++ OzonContest/6.Terminal/Program.cs
@@ -0,0 +1,23 @@
+﻿//Пример теста 1
+//Входные данные
+
+//4
+//otLLLrRuEe256LLLN
+//LRLUUDE
+//itisatest
+//abNcdLLLeUfNxNx
+
+//Выходные данные
+
+//route
+//256
+//-
+
+//-
+//itisatest
+//-
+//af
+//x
+//xb
+//ecd
+//-
