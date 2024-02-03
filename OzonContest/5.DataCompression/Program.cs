f8f9577232e3df0d2d50cc65be8d4f0626719fb6
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2024-02-03 22:09:06 +0300

    published 5.DataCompression
    127ms 22mb

--- OzonContest/5.DataCompression/Program.cs
+++ OzonContest/5.DataCompression/Program.cs
@@ -24,3 +24,51 @@
 //1 0 3 0 5 0 7 0 9 0 11 0 13 0
 //6
 //100 3 19 3 42 -2
+
+
+int amount = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < amount; i++)
+{
+    int inputListLength = int.Parse(Console.ReadLine());
+    string inputList = Console.ReadLine();
+
+    int[] array = Array.ConvertAll(inputList.Split(), int.Parse);
+    List<int> compressedList = new List<int>();
+    int tmp = 0;
+
+    for (int j = 0; j < inputListLength - 1; j++)
+    {
+        int firstValue = array[j];
+        int seccondValue = array[j + 1];
+
+        if (tmp == 0)
+        {
+            compressedList.Add(firstValue);
+        }
+        if (firstValue - seccondValue == 1 && tmp <= 0)
+        {
+            tmp--;
+        }
+        else if (firstValue - seccondValue == -1 && tmp >= 0)
+        {
+            tmp++;
+        }
+        else
+        {
+            compressedList.Add(tmp);
+            tmp = 0;
+        }
+    }
+    if (tmp != 0)
+    {
+        compressedList.Add(tmp);
+    }
+    else
+    {
+        compressedList.Add(array[inputListLength - 1]);
+        compressedList.Add(tmp);
+    }
+    Console.WriteLine(compressedList.Count);
+    Console.WriteLine(string.Join(" ", compressedList));
+}
