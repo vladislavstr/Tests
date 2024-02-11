253ee9dde73567645d7ce3c4152b56d2741b026b
VladW <VIStrelnikov94@gmail.com>
2024-02-11 03:24:49 +0300

    published 7.DocumentPrinter

--- OzonContest/7.DocumentPrinter/Program.cs
+++ OzonContest/7.DocumentPrinter/Program.cs
@@ -25,3 +25,66 @@
 //7
 //3
 //8-9,21-99
+
+using var inputAmount = new StreamReader(Console.OpenStandardInput());
+int amount = int.Parse(inputAmount.ReadLine());
+for (int i = 0; i < amount; i++)
+{
+    using var inputCountOfPages = new StreamReader(Console.OpenStandardInput());
+    var CountOfPages = int.Parse(inputCountOfPages.ReadLine());
+    HashSet<int> setOfPages = new HashSet<int>();
+
+    for (int j = 1; j <= CountOfPages; j++)
+    {
+        setOfPages.Add(j);
+    }
+
+    using var inputNumberOfPages = new StreamReader(Console.OpenStandardInput());
+    string[] NumberOfPages = inputNumberOfPages.ReadLine().Split(',');
+
+    foreach (string item in NumberOfPages)
+    {
+        if (int.TryParse(item, out int number))
+        {
+            setOfPages.Remove(int.Parse(item));
+        }
+        else
+        {
+            int[] arrayOfPages = Array.ConvertAll(item.Split('-'), int.Parse);
+
+            int intervalBeginning = arrayOfPages[0];
+            int intervalEnding = arrayOfPages[1];
+
+            for (int n = intervalBeginning; n <= intervalEnding; n++)
+            {
+                setOfPages.Remove(n);
+            }
+        }
+    }
+
+    int[] arrayOfHalfBakedPages = setOfPages.OrderBy(x => x).ToArray();
+    int lengthOfHalfBakedPagesarray = arrayOfHalfBakedPages.Length;
+
+    List<string> needToPrint = new List<string>();
+
+    for (int k = 0; k < lengthOfHalfBakedPagesarray; k++)
+    {
+        if (k + 1 < lengthOfHalfBakedPagesarray && arrayOfHalfBakedPages[k + 1] - arrayOfHalfBakedPages[k] == 1)
+        {
+            int firstValue = arrayOfHalfBakedPages[k];
+            while (k + 1 < lengthOfHalfBakedPagesarray && arrayOfHalfBakedPages[k + 1] - arrayOfHalfBakedPages[k] == 1)
+            {
+                k++;
+            }
+            int seccondValue = arrayOfHalfBakedPages[k];
+            needToPrint.Add($"{firstValue}-{seccondValue}");
+        }
+        else
+        {
+            needToPrint.Add($"{arrayOfHalfBakedPages[k]}");
+        }
+    }
+
+    using var output = new StreamWriter(Console.OpenStandardOutput());
+    output.WriteLine(string.Join(",", needToPrint.ToArray()));
+}
