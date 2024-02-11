732e439d06b9e18aa599e9b3d7230239d8cb4d93
VladW <VIStrelnikov94@gmail.com>
2024-02-11 04:22:39 +0300

    published

--- OzonContest/2.DateChecker/Program.cs
+++ OzonContest/2.DateChecker/Program.cs
@@ -26,16 +26,43 @@
 //YES
 //NO
 
-int amount = int.Parse(Console.ReadLine());
+using var input = new StreamReader(Console.OpenStandardInput());
+using var output = new StreamWriter(Console.OpenStandardOutput());
+
+int amount = int.Parse(input.ReadLine());
 for (int i = 0; i < amount; i++)
 {
-    string dateInput = Console.ReadLine();
-
-    bool result = DateTime.TryParse(dateInput, out var number);
-    if (result == true)
+    string date = input.ReadLine();
+    List<int> a = date.Split(' ').Select(x => int.Parse(x)).ToList();
+    var d = a[0];
+    var m = a[1];
+    var y = a[2];
+    bool isValid = d >= 1 && d <= 31 && m >= 1 && m <= 12 && y >= 1950 && y <= 2300;
+    if (isValid)
     {
-        Console.WriteLine("YES");
+        if (m == 2)
+        {
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                isValid = d >= 1 && d <= 29;
+            }
+            else
+            {
+                isValid = d >= 1 && d <= 28;
+            }
+        }
+        else
+        {
+            if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+            {
+                isValid = d >= 1 && d <= 31;
+            }
+            else
+            {
+                isValid = d >= 1 && d <= 30;
+            }
+        }
     }
-    else
-        Console.WriteLine("NO");
+
+    output.WriteLine(isValid ? "YES" : "NO");
 }
